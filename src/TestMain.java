25ea237fe479ac19e9536aa9ca6cf1bd2cc80d54
EShad <romuald.rousseau.35@gmail.com>
2019-01-06 00:03:58 +0100

    Ajout d'attributs

--- src/TestMain.java
+++ src/TestMain.java
@@ -1,8 +1,37 @@
 
 public class TestMain {
 
+	String nom;
+	String prenom;
+	int age;
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		// Implémenter la méthode
 
 	}
 
